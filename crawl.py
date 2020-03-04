bb192770fe52322158718534f70b035488d89dcb
Rijin Mk <rijin.mk9@gmail.com>
2020-03-04 11:48:54 +0400

    Created a URL Object

--- crawl.py
+++ crawl.py
@@ -4,6 +4,13 @@ import random
 import urllib 
 import os
 import sys
+import urlparse
 
+if(len(sys.argv) > 1):
+    print "Validating URL:", sys.argv[1]
 
+    o = urlparse.urlparse(sys.argv[1])
+    print o
 
+else: 
+    print "Please provide a URL"
